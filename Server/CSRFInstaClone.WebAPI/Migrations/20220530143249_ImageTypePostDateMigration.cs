d7cfc58c7944deea1ff891dac3150bfa5e01d821
DerMagereStudent <malo0466@gmail.com>
2022-05-30 16:37:06 +0200

    Commit Bug. Migration missing

---
+++ Server/CSRFInstaClone.WebAPI/Migrations/20220530143249_ImageTypePostDateMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSRFInstaClone.WebAPI.Migrations
+{
+    public partial class ImageTypePostDateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTimePosted",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageType",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateTimePosted",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ImageType",
+                table: "Images");
+        }
+    }
+}
